90a3ba9393e472ed6087995fe5400077e501e423
KellyHdz <98137897+KellyHdz@users.noreply.github.com>
2023-10-17 18:28:23 -0600

    Add files via upload

---
+++ p2_.py
@@ -0,0 +1,15 @@
+import requests
+import io
+from bs4 import BeautifulSoup
+import pandas as pd
+from tabulate import tabulate
+from typing import Tuple, List
+import re
+from datetime import datetime
+
+
+data = pd.read_csv("DatosCovid.csv")
+new_data = data
+
+new_data.dropna(inplace=True)
+new_data.to_csv("limpiezaCSV_.csv",index=False)
