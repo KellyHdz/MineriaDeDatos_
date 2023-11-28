37d7f30d8da231e929ffc1bab1a7187033676026
KellyHdz <98137897+KellyHdz@users.noreply.github.com>
2023-11-27 19:14:25 -0600

    practica1.py

--- practica1.py
+++ practica1.py
@@ -6,5 +6,5 @@ def get_csv_from_url(url: str) -> pd.DataFrame:
     r = requests.get(url).content
     return pd.read_csv(io.StringIO(r.decode('utf-8')))
 
-df = get_csv_from_url("https://raw.githubusercontent.com/KellyHdz/csv_MineriaDeDatos/main/csv.csv?token=GHSAT0AAAAAACHJWA33K5S7366GQXKZJHTEZJVXZ5Q")
+df = get_csv_from_url("https://raw.githubusercontent.com/KellyHdz/csv_MineriaDeDatos/main/CSV.csv?token=GHSAT0AAAAAACK3WDB7U5Y5KU46DM5BOWK2ZLFH3NQ")
 df.to_csv("CSV.csv", index=False)
